530a21c7f431831f668c555c1c07151f93882130
Rick Dillon <rdillon.enservio@gmail.com>
2016-03-10 20:46:01 -0500

    initial commit of project

---
+++ js/playpause.js
@@ -0,0 +1,21 @@
+(function(){ // start IIFE
+
+	var increment, step, rotate;
+	var top, bottom, rotate;
+	var $bgCircle;
+
+	init = function(){
+		increment = 0;
+		step = 0;
+		rotate = 0;
+		// apply default styles
+		
+		$bgCircle = $('#bgCircle')
+
+		console.log($bgCircle);
+
+	}
+
+	init()
+
+})(); // end IIFE
