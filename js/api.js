072f1874c2cb39c48c93195c928c17252c27e2dd
Rick Dillon <rdillon@rdillon-mac.home>
2016-03-12 10:03:03 -0500

    subject line
    what happened
    
    [ticket: X]
    Adding comments to api.js for the sake of testing the git template config

--- js/api.js
+++ js/api.js
@@ -1,4 +1,8 @@
 /*
+ *
+ * the rotating play/pause object has 4 animated states
+ * that can be triggered by user actions (e.g., touch/click)
+ * and triggered by system events (e.g., socketMsg)
  * 
  */
  var pp
