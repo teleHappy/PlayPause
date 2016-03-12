ba2aae7923e71e48b308b9ba2f5fda6e71fb7994
Rick Dillon <rdillon@rdillon-mac.home>
2016-03-12 10:06:34 -0500

    Details: More updates to api.js comments in order to test the git template

--- js/api.js
+++ js/api.js
@@ -2,7 +2,9 @@
  *
  * the rotating play/pause object has 4 animated states
  * that can be triggered by user actions (e.g., touch/click)
- * and triggered by system events (e.g., socketMsg)
+ * and/or triggered by system events (e.g., socketMsg)
+ * 
+ * the api must support these triggers
  * 
  */
  var pp
