7b5f9f90cf94db5f1d208cf56cab9625655a9adf
Rick Dillon <rdillon@rdillon-mac.home>
2016-03-13 00:03:23 -0500

    Subject: Replaced setTimeout with click handler

--- js/playpause.js
+++ js/playpause.js
@@ -1,94 +1,79 @@
-(function(){ // start IIFE
+$('document').ready(function(){
 
-	var increment, step, rotate;
-	var top, bottom, rotate;
-	var $bgCircle;
-
-	init = function(){
-		increment = 0;
-		step = 0;
-		rotate = 0;
-		// apply default styles
-		
-		$bgCircle = $('#bgCircle')
-
-		console.log($bgCircle);
-
-	}
-
-	init();
-
-	// global for now
-var increment = 0; 
-
-var setInt = setInterval(function(){
-  
-  var step = increment % 4;
-  var rotate;
+  var step = increment = rotate = 0; // 4 possible steps based on increment % 4
+  var incremental = 1;
+  var $bgCircle = $('#bgCircle');
   var $top = $('#top');
   var $bottom = $('#bottom');
   var $rotate = $('#rotate');
-  
-  // console.log(step)
 
-  // pause 0
-  if(step === 0){
+  $bgCircle.on('click', togglePlayPause);
+
+  function reset(){
+    step = increment = rotate = 0;
     
-    increment++;
-    rotate = 90 * increment; // 90
-
     $top.removeClass();
     $bottom.removeClass();
 
-  	$top.addClass('pauseTop_0');
-  	$bottom.addClass('pauseBottom_0');
-
-   	$rotate.css({transform: 'rotate('+rotate+'deg)'})
+    $rotate.css({transform: 'rotate('+rotate+'deg)'});
   }
 
-  // play 1
-  else if(step === 1){
+  function togglePlayPause(){
+    // default state is play0 - user or system has not yet initiated playback
+   // debugger
+    if(incremental%4 === 1){
 
-    increment++;
-    rotate = 90 * increment; // 180
+      rotate = 90 * incremental;
 
-    $top.removeClass();
-    $bottom.removeClass();
+      $top.removeClass();
+      $bottom.removeClass();
 
-    $top.addClass('playTop_01');
-    $bottom.addClass('playBottom_01');
+      $top.addClass('pauseTop_0');
+      $bottom.addClass('pauseBottom_0');
+
+      $rotate.css({transform: 'rotate('+rotate+'deg)'})
+
+    }
+    if(incremental%4 === 2){
+
+      rotate = 90 * incremental; // 180
+
+      $top.removeClass();
+      $bottom.removeClass();
+
+      $top.addClass('playTop_01');
+      $bottom.addClass('playBottom_01');
+
+      $rotate.css({transform: 'rotate('+rotate+'deg)'})
+
+    }
+    if(incremental%4 === 3){
+
+      rotate = 90 * incremental; // 270
+
+      $top.removeClass();
+      $bottom.removeClass();
+
+      $top.addClass('pauseTop_02');
+      $bottom.addClass('pauseBottom_02');
+
+      $rotate.css({transform: 'rotate('+rotate+'deg)'})
+
+    }
+    if(incremental%4 === 0){
+
+      rotate = 90 * incremental; // 360
+
+      $top.removeClass();
+      $bottom.removeClass();
+
+      $rotate.css({transform: 'rotate('+rotate+'deg)'})
+
+    }
+
+    incremental++;
+  }
+
+});
 	
-    $rotate.css({transform: 'rotate('+rotate+'deg)'})
-    
-  }
 
-  // pause 2
-  else if(step === 2){
-
-    increment++;
-    rotate = 90 * increment; // 270
-
-    $top.removeClass();
-    $bottom.removeClass();
-
-    $top.addClass('pauseTop_02');
-    $bottom.addClass('pauseBottom_02');
-
-    $rotate.css({transform: 'rotate('+rotate+'deg)'})
-  
-  }
-  // play 3, same as initial state
-  else if(step === 3){
-
-    increment++;
-    rotate = 90 * increment; // 360
-
-    $top.removeClass();
-    $bottom.removeClass();
-
-    $rotate.css({transform: 'rotate('+rotate+'deg)'})
-  
-  }
-}, 2000);
-
-})(); // end IIFE
